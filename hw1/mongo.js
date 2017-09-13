71e3c07dec54b08f337dc90b90a86bc023e53ec5
Zack Hartmann <zackhartmann@gmail.com>
2017-09-13 16:35:53 -0400

    Goodness

--- hw1/mongo.js
+++ hw1/mongo.js
@@ -1,19 +1,27 @@
-// This is Not Run
-// No semi-colons
+// Create Collection
 db.createCollection("monsters")
 
-db.monsters.insert( {
+var monster1 = {
 	"name" : "Godzilla",
 	"health" : 100,
-	"lastfought" : "1969"},
-	{"attacks" : ["bite", "stomp", "thrash"]},
-	{"stats" : {"attack" : 60, "defense": 50}
-} )
-
-db.monsters.insert( {
+	"lastfought" : "1969",
+	"attacks" : ["bite", "stomp", "thrash"],
+	"stats" : {"attack" : 60, "defense": 50}
+}
+var monster2 = {
 	"name" : "Cthulhu",
 	"health" : 100,
-	"lastfought" : "1069"},
-	{"attacks" : ["thrash", "smite", "suffocate"]},
-	{"stats" : {"attack" : 80, "defense": 70}
-} )
+	"lastfought" : "1069",
+	"attacks" : ["thrash", "smite", "suffocate"],
+	"stats" : {"attack" : 80, "defense": 70}
+}
+
+function createMonster(name,health,lastfought,attacks,stats) {
+	db.database.insert( {
+			"name" : name,
+			"health" : health,
+			"lastfought" : lastfought,
+			"attacks" : attacks,
+			"stats" : stats
+	} )
+}
