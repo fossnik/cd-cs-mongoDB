5a5fc020e3b2049cee7276ae3e75204b3f0ea3ff
Zack Hartmann <zackhartmann@gmail.com>
2017-09-13 16:50:37 -0400

    find by name

--- hw1/mongo.js
+++ hw1/mongo.js
@@ -27,9 +27,14 @@ function createMonster(name,health,lastfought,attacks,stats,db) {
 }
 
 // Write the javascript code that would insert the four attributes into the “monster” collection.
- createMonster(monster1.name, monster1.health, monster1.lastfought, monster1.attacks, monster1.stats, "monsters");
+createMonster(monster1.name, monster1.health, monster1.lastfought, monster1.attacks, monster1.stats, "monsters");
 
 // Write the code that would then console.log all the documents inside the collection.
 console.log(
 	db.monsters.find({})
 )
+
+// Write the command that will find your monster based on it’s name
+function findMonster(name){
+	db.monsters.find({"name": name})
+}
