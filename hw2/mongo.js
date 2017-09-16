7b8ebff25eeab1055b58c38bb76e365e6ac2e837
Zack Hartmann <zackhartmann@gmail.com>
2017-09-16 10:36:02 -0400

    Questions

--- hw2/mongo.js
+++ hw2/mongo.js
@@ -14,3 +14,11 @@
 // var mongo = function(db) {
 //
 // };
+
+// Write the command that will find all monsters with an attack lower than 10
+// Write the command that will find all monsters with a level above 5 but below 15 inclusive
+// Write the command to find monsters that do not have the “bite” attack.
+// Write the command to find monsters with levels less than 6 but only return the name, level and health attributes
+// Write the command to find all monsters who attack is between 10 and 20 but do not include the monsters health or style
+// Write the command to find out how many monsters are in the collection.
+// Write the command to sort the collection by the monsters level with the highest level at the top and the lowest at the bottom
