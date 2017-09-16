32808290b922a1588ad7ea0b31a063ce98790254
Zack Hartmann <zackhartmann@gmail.com>
2017-09-16 11:07:27 -0400

    Question2

--- hw2/mongo.js
+++ hw2/mongo.js
@@ -20,6 +20,9 @@ var question1 = function(db) {
 	db.monsters.find({"stats.attack":{"$lt": 10}})
 }
 // Write the command that will find all monsters with a level above 5 but below 15 inclusive
+var question2 = function(db) {
+	db.monsters.find({"stats.attack":{"$gt": 10},{"$lte":15}})
+}
 // Write the command to find monsters that do not have the “bite” attack.
 // Write the command to find monsters with levels less than 6 but only return the name, level and health attributes
 // Write the command to find all monsters who attack is between 10 and 20 but do not include the monsters health or style
