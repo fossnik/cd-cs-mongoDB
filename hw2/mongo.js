0b1928e0e041d0a34cfc6de31febf6a5becfcc98
Zack Hartmann <zackhartmann@gmail.com>
2017-09-16 10:32:36 -0400

    File

---
+++ hw2/mongo.js
@@ -0,0 +1,16 @@
+// {
+//     "name": "Bat",
+//     "Level": 4,
+//     "health": 25,
+//     "type": "Flying",
+//     "attacks": ["scratch", "bite"],
+//     "stats": {
+//         "attack": 5,
+//         "defense": 2,
+//     }
+//     "style": "cool"
+// }
+
+// var mongo = function(db) {
+//
+// };
